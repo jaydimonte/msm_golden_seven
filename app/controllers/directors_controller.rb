5c3953ff9d21f1b169eb77db71132d1d4c40cac5
Jackie DiMonte <jdimonte@chicagobooth.edu>
2017-07-22 16:38:52 +0000

    partial - 2

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,45 +3,45 @@ class DirectorsController < ApplicationController
     @directors = Director.all
   end
 
-#   def show
-#     @director = Director.find(params[:id])
-#   end
+  def show
+    @director = Director.find(params[:id])
+  end
 
-#   def new_form
-#   end
+  def new_form
+  end
 
-#   def create_row
-#     @director = Director.new
-#     @director.dob = params[:dob]
-#     @director.name = params[:name]
-#     @director.bio = params[:bio]
-#     @director.image_url = params[:image_url]
+  def create_row
+    @director = Director.new
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
-#     @director.save
+    @director.save
 
-#     render("show")
-#   end
+    render("show")
+  end
 
-#   def edit_form
-#     @director = Director.find(params[:id])
-#   end
+  def edit_form
+    @director = Director.find(params[:id])
+  end
 
-#   def update_row
-#     @director = Director.find(params[:id])
+  def update_row
+    @director = Director.find(params[:id])
 
-#     @director.dob = params[:dob]
-#     @director.name = params[:name]
-#     @director.bio = params[:bio]
-#     @director.image_url = params[:image_url]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
-#     @director.save
+    @director.save
 
-#     render("show")
-#   end
+    render("show")
+  end
 
-#   def destroy
-#     @director = Director.find(params[:id])
+  def destroy
+    @director = Director.find(params[:id])
 
-#     @director.destroy
-#   end
+    @director.destroy
+  end
 end
